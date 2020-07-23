647a6131a72893b1c87a5e6b2af124d95e5ae3c7
yangchen <chen.yang1@thoughtworks.com>
2020-07-23 20:11:26 +0800

    send message to looper using post method

--- app/src/main/java/com/example/handler/HandlerActivity.java
+++ app/src/main/java/com/example/handler/HandlerActivity.java
@@ -1,14 +1,46 @@
 package com.example.handler;
 
+import android.os.Bundle;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class HandlerActivity extends AppCompatActivity {
+
+    private MyHandlerThread myHandlerThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_handler);
+
+        ButterKnife.bind(this);
+        myHandlerThread = new MyHandlerThread();
+        myHandlerThread.start();
     }
+
+    @OnClick(R.id.button_android)
+    void setAndroid() {
+        myHandlerThread.handler.post(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(HandlerActivity.this, "android", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @OnClick(R.id.button_java)
+    void setJava() {
+        myHandlerThread.handler.post(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(HandlerActivity.this, "java", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
 }
---
+++ app/src/main/java/com/example/handler/MyHandlerThread.java
@@ -0,0 +1,16 @@
+package com.example.handler;
+
+import android.os.Handler;
+import android.os.Looper;
+
+public class MyHandlerThread extends Thread {
+
+    public Handler handler;
+
+    @Override
+    public void run() {
+        Looper.prepare();
+        handler = new Handler();
+        Looper.loop();
+    }
+}
