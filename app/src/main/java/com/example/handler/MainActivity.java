9a45cd26371921f29ab4a082663ca0a88f2579ed
yangchen <chen.yang1@thoughtworks.com>
2020-07-23 19:44:10 +0800

    add onclick event to button handler

--- app/src/main/java/com/example/handler/MainActivity.java
+++ app/src/main/java/com/example/handler/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(MainActivity.this);
+    }
+
+    @OnClick(R.id.handler_practices)
+    void onClick() {
+        Intent intent = new Intent(this, HandlerActivity.class);
+        startActivity(intent);
     }
 }
